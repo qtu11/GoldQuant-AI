cab511c73beefdd6fe05c0e7171dcb0e9cba1572
Chủ tịch Tú <boss@goldquant.ai>
2026-07-11 22:26:30 +0700

    chore: configuration of firebase dynamic environment variables

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDRmJ294aBZhVEcTliP86D19qM3tJbhgDI", 
-  authDomain: "qtusdev.firebaseapp.com", 
-  projectId: "qtusdev", 
-  storageBucket: "qtusdev.firebasestorage.app", 
-  messagingSenderId: "98060679867", 
-  appId: "1:98060679867:web:563015a0a052b2f432879e", 
-  measurementId: "G-F4F16X1TW4" 
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY, 
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN, 
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID, 
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET, 
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID, 
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID, 
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID 
 };
 
 // Khởi tạo Firebase
